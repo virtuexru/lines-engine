c27538bc558032ce37f0cf806cbb585d5d090cf6
Leon Shalimov <virtuexru@gmail.com>
2018-10-13 18:06:39 -0400

    fixing datepicker error

--- app/assets/javascripts/lines/admin/admin.js
+++ app/assets/javascripts/lines/admin/admin.js
@@ -1,6 +1,6 @@
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui/widgets/datepicker
+//= require jquery-ui/datepicker
 //= require autocomplete-rails
 //= require jquery-fileupload/basic
 //= require jquery-fileupload/vendor/tmpl
